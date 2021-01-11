e38952cd129af0d877dbb4a3e0279e706f968c70
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-11 13:17:15 -0500

    lecture work-in-progress

--- review/src/App2.js
+++ review/src/App2.js
@@ -16,8 +16,6 @@ class App2 extends React.Component {
     }
 
     render() {
-        const name = "Warren";
-
         return(<div>
             <h1>Hello {this.state.name}. You are {this.state.age}.</h1>
             <button onClick={this.handleClick}>Make it Allison instead.</button>
