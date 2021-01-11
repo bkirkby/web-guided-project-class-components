b12cbc04bcd9eeb01f98bf23b183a5128940c8d6
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-11 14:02:22 -0500

    lecture work-in-progress

--- followAlong/src/index.js
+++ followAlong/src/index.js
@@ -53,7 +53,7 @@ class App extends React.Component {
         if(item.id === itemId) {
           return {
             ...item,
-            purchased: true
+            purchased: !item.purchased
           }
         }
         return(item);
