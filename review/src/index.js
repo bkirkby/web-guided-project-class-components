d70f05241f0adb5c129b64b38ac1aacf80b4bf16
kirkby <kirkby@neighbor.com>
2021-05-04 20:28:23 -0600

    progress!!

--- review/src/index.js
+++ review/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles.scss';
 import AppFunc from './AppFunc';
 import AppClass from './AppClass';
-import App from './AppFunc2';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
