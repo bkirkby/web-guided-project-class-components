01930ceb01da7d4060c32a29e462d77446af8c0c
kirkby <kirkby@neighbor.com>
2021-05-04 21:19:41 -0600

    progress!!

--- followAlong/src/components/Item.js
+++ followAlong/src/components/Item.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Item = props => {
+  const handleClick = e => {
+    e.preventDefault();
+    props.togglePurchased(props.item.id);
+  }
   return (
     <div className={`item${props.item.purchased ? ' purchased' : ''}`}>
       <p>{props.item.name}</p>
