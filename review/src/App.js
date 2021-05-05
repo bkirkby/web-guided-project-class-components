e056a7225828d8cd214dff1b92e510ae00c0bb2a
kirkby <kirkby@neighbor.com>
2021-05-04 19:52:13 -0600

    progress!!

--- review/src/App.js
+++ review/src/App.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
 
+const Person = props => {
+  return (
+    <div>
+      <h1>Hello {props.name}!</h1>
+      <p>
+        {props.name} is a great name! never change it!
+      </p>
+    </div>
+};
+
 const App = () => {
   return (
     <div>
