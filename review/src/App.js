2a43d690c4fa1d85895c077f63a74b26f0ab2b3d
kirkby <kirkby@neighbor.com>
2021-05-04 20:23:01 -0600

    progress!!

---
+++ review/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Person = props => {
+  return (
+    <div>
+      <h1>hello {props.name}</h1>
+      <p>
+        {props.name} is a great name. never change it!
+      </p>
+    </div>
+  )
+}
+
+class App extends React.Component {
+
+  handleSomething = () => {
+
+  }
+
+  // handleSomethingElse() {
+  // }
+
+  render() {
+    return (
+      <div>
+        <Person name={''} />
+      </div>
+    )
+  }
+}
